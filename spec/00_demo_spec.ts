9fe125fab0c1f94091d541a47e171eddb2842583
jcicero02 <jcicero02@gmail.com>
2019-03-13 15:55:52 -0400

    end of day 2

---
+++ spec/00_demo_spec.ts
@@ -0,0 +1,11 @@
+describe('Writing a Spec', () => {
+    it('is easy when you know how!', () => {
+
+        // GIVEN - ARRANGE
+        const a = 10, b = 20;
+        //WHEN - ACT
+        const answer = a + b;
+        //THEN - ASSERT
+        expect(answer).toBe(30)
+    });
+});
